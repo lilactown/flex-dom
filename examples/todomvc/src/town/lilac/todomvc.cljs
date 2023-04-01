e8d09359ab00e2b4299ff7c2935b649b7580ac25
Will Acton <will@willacton.com>
2023-04-01 14:00:42 -0700

    todomvc: mock fetch of initial todos

--- examples/todomvc/src/town/lilac/todomvc.cljs
+++ examples/todomvc/src/town/lilac/todomvc.cljs
@@ -1,7 +1,8 @@
 (ns town.lilac.todomvc
   (:require
    [town.lilac.flex :as flex]
-   [town.lilac.flex.dom :as dom]))
+   [town.lilac.flex.dom :as dom]
+   [town.lilac.flex.promise :as flex.p]))
 
 (defn todo-item
   [{:keys [title id completed?]}
@@ -57,104 +58,123 @@
                        :on-delete #(on-delete (:id todo))
                        :on-edit #(on-edit (:id todo) %)})))))
 
-(defn app
-  []
-  (let [ids (flex/source 2)
-        todos (flex/source [{:id 0
+;; Mock fetching some initial state
+(defonce initial-todos
+  (flex.p/resource #(js/Promise.
+                     (fn [res _]
+                       (js/setTimeout
+                        (fn []
+                          (res
+                           [{:id 0
                              :title "Write code"
                              :completed? true}
                             {:id 1
                              :title "Dishes"
-                             :completed? false}])
-        list-filter (flex/source :all)]
-    (dom/section
-     {:class "todoapp"}
+                             :completed? false}]))
+                        2000)))))
+
+(defn app
+  []
+  (dom/section
+   {:class "todoapp"}
+   (if (doto @(:loading? initial-todos) prn)
      (dom/header
       {:class "header"}
       (dom/h1 (dom/text "todos"))
-      (dom/form
-       {:onsubmit (fn [^js e]
-                    (.preventDefault e)
-                    (flex/batch
-                     (let [id @ids]
-                       (ids inc)
-                       (todos #(conj % {:id id
-                                        :title (.. e -target -title -value)
-                                        :completed? false}))))
-                    (set! (.. e -target -title -value) ""))}
-       (dom/input {:type "text"
-                   :class "new-todo"
-                   :name "title"
-                   :autofocus true
-                   :autocomplete "off"
-                   :placeholder "What needs to be done?"})))
-     (todo-list
-      {:filter list-filter
-       :todos todos
-       :on-toggle (fn [id]
-                    (todos (fn [todos]
-                             (mapv
-                              #(if (= id (:id %))
-                                 (update % :completed? not)
-                                 %)
-                              todos))))
-       :on-delete (fn [id]
-                    (todos (fn [todos]
-                             (->> todos
-                                  (filter #(not= id (:id %)))
-                                  vec))))
-       :on-toggle-all (fn [v]
-                        (todos
-                         (fn [todos]
-                           (mapv #(assoc % :completed? v) todos))))
-       :on-edit (fn [id v]
-                  (todos
-                   (fn [todos]
-                     (mapv
-                      #(if (= id (:id %))
-                         (assoc % :title v)
-                         %)
-                      todos))))})
-     (dom/footer
-      {:class "footer"}
-      (dom/span
-       {:class "todo-count"}
-       (dom/strong
-        (dom/text
-         (str (count (filter #(not (:completed? %)) @todos))
-              " item(s) left"))))
-      (dom/ul
-       {:class "filters"}
-       (dom/li (dom/a
-                {:class (if (= @list-filter :all) "selected" "")
-                 :href "#"
-                 :onclick (fn [e]
-                            (.preventDefault e)
-                            (list-filter :all))}
-                (dom/text "All")))
-       (dom/li (dom/a
-                {:class (if (= @list-filter :active) "selected" "")
-                 :href "#"
-                 :onclick (fn [e]
-                            (.preventDefault e)
-                            (list-filter :active))}
-                (dom/text "Active")))
-       (dom/li (dom/a
-                {:class (if (= @list-filter :complete) "selected" "")
-                 :href "#"
-                 :onclick (fn [e]
-                            (.preventDefault e)
-                            (list-filter :complete))}
-                (dom/text "Complete"))))
-      (dom/button
-       {:class "clear-completed"
-        :onclick (fn [_e]
-                   (todos
-                    (fn [todos]
-                      (->> todos
-                           (filter #(not (:completed? %)))
-                           (vec)))))}
-       (dom/text "Clear completed"))))))
+      (dom/div
+       {:style {:text-align "center"
+                :padding "20px"
+                :font-size "24px"}}
+       (dom/text "Loading...")))
+     (let [ids (flex/source 2)
+           todos (flex/source @initial-todos)
+           list-filter (flex/source :all)]
+       (dom/header
+        {:class "header"}
+        (dom/h1 (dom/text "todos"))
+        (dom/form
+         {:onsubmit (fn [^js e]
+                      (.preventDefault e)
+                      (flex/batch
+                       (let [id @ids]
+                         (ids inc)
+                         (todos #(conj % {:id id
+                                          :title (.. e -target -title -value)
+                                          :completed? false}))))
+                      (set! (.. e -target -title -value) ""))}
+         (dom/input {:type "text"
+                     :class "new-todo"
+                     :name "title"
+                     :autofocus true
+                     :autocomplete "off"
+                     :placeholder "What needs to be done?"})))
+       (todo-list
+        {:filter list-filter
+         :todos todos
+         :on-toggle (fn [id]
+                      (todos (fn [todos]
+                               (mapv
+                                #(if (= id (:id %))
+                                   (update % :completed? not)
+                                   %)
+                                todos))))
+         :on-delete (fn [id]
+                      (todos (fn [todos]
+                               (->> todos
+                                    (filter #(not= id (:id %)))
+                                    vec))))
+         :on-toggle-all (fn [v]
+                          (todos
+                           (fn [todos]
+                             (mapv #(assoc % :completed? v) todos))))
+         :on-edit (fn [id v]
+                    (todos
+                     (fn [todos]
+                       (mapv
+                        #(if (= id (:id %))
+                           (assoc % :title v)
+                           %)
+                        todos))))})
+       (dom/footer
+        {:class "footer"}
+        (dom/span
+         {:class "todo-count"}
+         (dom/strong
+          (dom/text
+           (str (count (filter #(not (:completed? %)) @todos))
+                " item(s) left"))))
+        (dom/ul
+         {:class "filters"}
+         (dom/li (dom/a
+                  {:class (if (= @list-filter :all) "selected" "")
+                   :href "#"
+                   :onclick (fn [e]
+                              (.preventDefault e)
+                              (list-filter :all))}
+                  (dom/text "All")))
+         (dom/li (dom/a
+                  {:class (if (= @list-filter :active) "selected" "")
+                   :href "#"
+                   :onclick (fn [e]
+                              (.preventDefault e)
+                              (list-filter :active))}
+                  (dom/text "Active")))
+         (dom/li (dom/a
+                  {:class (if (= @list-filter :complete) "selected" "")
+                   :href "#"
+                   :onclick (fn [e]
+                              (.preventDefault e)
+                              (list-filter :complete))}
+                  (dom/text "Complete"))))
+        (dom/button
+         {:class "clear-completed"
+          :onclick (fn [_e]
+                     (todos
+                      (fn [todos]
+                        (->> todos
+                             (filter #(not (:completed? %)))
+                             (vec)))))}
+         (dom/text "Clear completed")))))))
 
 (defonce root
   (dom/create-root
