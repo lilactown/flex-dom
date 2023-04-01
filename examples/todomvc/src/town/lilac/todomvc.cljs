1ccb493aff530be3f3bc0c471b1269519c4dfffe
Will Acton <will@willacton.com>
2023-04-01 12:00:29 -0700

    get rid of need for scope

--- examples/todomvc/src/town/lilac/todomvc.cljs
+++ examples/todomvc/src/town/lilac/todomvc.cljs
@@ -1,41 +1,39 @@
 (ns town.lilac.todomvc
   (:require
-   [town.lilac.dom :as dom]
    [town.lilac.flex :as flex]
-   [town.lilac.flex.dom :as flex.dom]))
+   [town.lilac.flex.dom :as dom]))
 
 (defn todo-item
   [{:keys [title id completed?]}
    {:keys [on-toggle on-delete on-edit]}]
   (let [editing? (flex/source false)]
-    (flex.dom/scope
-     (dom/li
-      {:class (if @editing? "todo editing" "todo") :id id :key id}
-      (dom/div
-       {:class "view"}
-       (let [el (dom/input {:class "toggle"
-                            :type "checkbox"
-                            :checked (when completed? true)
-                            :name "completed"
-                            :onchange #(on-toggle)})]
-         (when (not= completed? (= "on" (.. el -checked)))
-           (set! (.. el -checked) (case completed?
-                                    true "on"
-                                    false nil))))
-       (dom/label
-        {:ondblclick #(editing? true)}
-        (dom/text title))
-       (dom/button {:class "destroy"
-                    :onclick #(on-delete)}))
-      (dom/form
-       {:onsubmit (fn [e]
-                    (.preventDefault e)
-                    (on-edit (.. e -target -title -value)))}
-       (dom/input {:class "edit"
-                   :type "text"
-                   :name "title"
-                   :autofocus true
-                   :value title}))))))
+    (dom/li
+     {:class (if @editing? "todo editing" "todo") :id id :key id}
+     (dom/div
+      {:class "view"}
+      (let [el (dom/input {:class "toggle"
+                           :type "checkbox"
+                           :checked (when completed? true)
+                           :name "completed"
+                           :onchange #(on-toggle)})]
+        (when (not= completed? (= "on" (.. el -checked)))
+          (set! (.. el -checked) (case completed?
+                                   true "on"
+                                   false nil))))
+      (dom/label
+       {:ondblclick #(editing? true)}
+       (dom/text title))
+      (dom/button {:class "destroy"
+                   :onclick #(on-delete)}))
+     (dom/form
+      {:onsubmit (fn [e]
+                   (.preventDefault e)
+                   (on-edit (.. e -target -title -value)))}
+      (dom/input {:class "edit"
+                  :type "text"
+                  :name "title"
+                  :autofocus true
+                  :value title})))))
 
 (defn todo-list
   [{:keys [filter todos on-toggle on-delete on-toggle-all on-edit]}]
@@ -45,13 +43,13 @@
                :class "toggle-all"
                :type "checkbox"
                :name "toggle-all"
-               :checked (when (every? #(:completed? %) todos) true)
+               :checked (when (every? #(:completed? %) @todos) true)
                :onchange #(on-toggle-all (.. % -target -checked))})
    (dom/label {:for "toggle-all"} (dom/text "Mark all as complete"))
    (dom/ul
     {:class "todo-list"}
-    (doseq [todo todos
-            :when (case filter
+    (doseq [todo @todos
+            :when (case @filter
                     :all true
                     :active (not (:completed? todo))
                     :complete (:completed? todo))]
@@ -69,6 +67,7 @@
                              :title "Dishes"
                              :completed? false}])
         list-filter (flex/source :all)]
+    (prn :app)
     (dom/section
      {:class "todoapp"}
      (dom/header
@@ -90,67 +89,64 @@
                    :autofocus true
                    :autocomplete "off"
                    :placeholder "What needs to be done?"})))
-     (flex.dom/scope
-      (todo-list
-       {:filter @list-filter
-        :todos @todos
-        :on-toggle (fn [id]
-                     (todos (fn [todos]
-                              (mapv
-                               #(if (= id (:id %))
-                                  (update % :completed? not)
-                                  %)
-                               todos))))
-        :on-delete (fn [id]
-                     (todos (fn [todos]
-                              (->> todos
-                                   (filter #(not= id (:id %)))
-                                   vec))))
-        :on-toggle-all (fn [v]
-                         (todos
-                          (fn [todos]
-                            (mapv #(assoc % :completed? v) todos))))
-        :on-edit (fn [id v]
-                   (todos
-                    (fn [todos]
-                      (mapv
-                       #(if (= id (:id %))
-                          (assoc % :title v)
-                          %)
-                       todos))))}))
+     (todo-list
+      {:filter list-filter
+       :todos todos
+       :on-toggle (fn [id]
+                    (todos (fn [todos]
+                             (mapv
+                              #(if (= id (:id %))
+                                 (update % :completed? not)
+                                 %)
+                              todos))))
+       :on-delete (fn [id]
+                    (todos (fn [todos]
+                             (->> todos
+                                  (filter #(not= id (:id %)))
+                                  vec))))
+       :on-toggle-all (fn [v]
+                        (todos
+                         (fn [todos]
+                           (mapv #(assoc % :completed? v) todos))))
+       :on-edit (fn [id v]
+                  (todos
+                   (fn [todos]
+                     (mapv
+                      #(if (= id (:id %))
+                         (assoc % :title v)
+                         %)
+                      todos))))})
      (dom/footer
       {:class "footer"}
-      (flex.dom/scope
-       (dom/span
-        {:class "todo-count"}
-        (dom/strong
-         (dom/text
-          (str (count (filter #(not (:completed? %)) @todos))
-               " item(s) left")))))
-      (flex.dom/scope
-       (dom/ul
-        {:class "filters"}
-        (dom/li (dom/a
-                 {:class (if (= @list-filter :all) "selected" "")
-                  :href "#"
-                  :onclick (fn [e]
-                             (.preventDefault e)
-                             (list-filter :all))}
-                 (dom/text "All")))
-        (dom/li (dom/a
-                 {:class (if (= @list-filter :active) "selected" "")
-                  :href "#"
-                  :onclick (fn [e]
-                             (.preventDefault e)
-                             (list-filter :active))}
-                 (dom/text "Active")))
-        (dom/li (dom/a
-                 {:class (if (= @list-filter :complete) "selected" "")
-                  :href "#"
-                  :onclick (fn [e]
-                             (.preventDefault e)
-                             (list-filter :complete))}
-                 (dom/text "Complete")))))
+      (dom/span
+       {:class "todo-count"}
+       (dom/strong
+        (dom/text
+         (str (count (filter #(not (:completed? %)) @todos))
+              " item(s) left"))))
+      (dom/ul
+       {:class "filters"}
+       (dom/li (dom/a
+                {:class (if (= @list-filter :all) "selected" "")
+                 :href "#"
+                 :onclick (fn [e]
+                            (.preventDefault e)
+                            (list-filter :all))}
+                (dom/text "All")))
+       (dom/li (dom/a
+                {:class (if (= @list-filter :active) "selected" "")
+                 :href "#"
+                 :onclick (fn [e]
+                            (.preventDefault e)
+                            (list-filter :active))}
+                (dom/text "Active")))
+       (dom/li (dom/a
+                {:class (if (= @list-filter :complete) "selected" "")
+                 :href "#"
+                 :onclick (fn [e]
+                            (.preventDefault e)
+                            (list-filter :complete))}
+                (dom/text "Complete"))))
       (dom/button
        {:class "clear-completed"
         :onclick (fn [_e]
@@ -162,7 +158,9 @@
        (dom/text "Clear completed"))))))
 
 (defonce root
-  (flex.dom/create-root (js/document.getElementById "root") #(app)))
+  (dom/create-root
+   (js/document.getElementById "root")
+   #(do (prn :root) (#'app))))
 
 (defn ^:dev/before-load start!
   []
--- src/town/lilac/flex/dom.clj
+++ src/town/lilac/flex/dom.clj
@@ -1,7 +1,8 @@
 (ns town.lilac.flex.dom
   (:require
    [town.lilac.flex :as flex]
-   [town.lilac.dom :as dom]))
+   [town.lilac.dom :as dom])
+  (:refer-clojure :exclude [map meta time]))
 
 (defmacro scope
   [& body]
@@ -10,3 +11,73 @@
       ([] (pfn#))
       ([el#]
        (dom/patch-outer el# pfn#)))))
+
+
+(defn reactive-body?
+  [body]
+  (cond
+    (seq? body) (when-not (= `$ (first body))
+                  (some reactive-body? body))
+    (coll? body) (some reactive-body? body)
+    :else (= `deref body)))
+
+
+(defmacro $
+  [sym & body]
+  (let [body `(~(symbol "town.lilac.dom" (name sym))
+               ~@body)]
+    (if (reactive-body? body)
+      `(scope ~body)
+      body)))
+
+(comment
+  (reactive-body?
+   `(let [foo @bar]
+      (dom/text "baz")))
+
+  (reactive-body?
+   `(let [foo bar]
+      ($ div @foo)))
+
+  (macroexpand '($ div ($ text "hi")))
+
+  (macroexpand '($ text @foo)))
+
+
+(declare
+ input textarea option select a abbr address area article aside audio b base bdi
+ bdo big blockquote body br button canvas caption cite code col colgroup data datalist
+ dd del details dfn dialog div dl dt em embed fieldset figcaption figure footer form
+ h1 h2 h3 h4 h5 h6 head header hr html i iframe img ins kbd keygen label legend li link
+ main map mark menu menuitem meta meter nav noscript object ol optgroup output p param
+ picture pre progress q rp rt ruby s samp script section small source span strong style
+ sub summary sup table tbody td tfoot th thead time title tr track u ul var video wbr
+ circle clipPath ellipse g line mask path pattern polyline rect svg defs
+ linearGradient polygon radialGradient stop tspan text)
+
+(def tags
+  '[input textarea option select a abbr address area article aside audio
+    b base bdi bdo big blockquote body br button canvas caption cite code col
+    colgroup data datalist dd del details dfn dialog div dl dt em embed fieldset
+    figcaption figure footer form h1 h2 h3 h4 h5 h6 head header hr html i iframe
+    img ins kbd keygen label legend li link main map mark menu menuitem meta
+    meter nav noscript object ol optgroup output p param picture pre progress q
+    rp rt ruby s samp script section small source span strong style sub summary
+    sup table tbody td tfoot th thead time title tr track u ul var video wbr
+    text])
+
+(defn gen-tag
+  [tag]
+  `(defmacro ~tag [& args#]
+     `($ ~(str '~tag) ~@args#)))
+
+(defmacro gen-tags
+  []
+  `(do
+     ~@(for [tag tags]
+         (gen-tag tag))))
+
+(gen-tags)
+
+(comment
+  (macroexpand `(div @foo)))
